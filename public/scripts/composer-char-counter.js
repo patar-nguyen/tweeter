1058f9432cf7e10e10c2d0c660f7bc6e29672673
Patrick Nguyen <patar.nguyen@gmail.com>
2022-02-01 16:21:02 -0500

    Adding Character Counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("form").find("textarea").keyup(function() {
+    const input = $(this).val().length;
+
+    $(this).next().find("output").text(140 - input);
+
+    if(input > 140) {
+      $(this).next().find("output").css("color", "red");
+    } else {
+      $(this).next().find("output").css("color", "#545149");
+    }
+  })
+});
