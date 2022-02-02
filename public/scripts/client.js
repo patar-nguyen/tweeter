81bc16ba7c80d7ab28a9d7a74249eddba086d6b3
Patrick Nguyen <patar.nguyen@gmail.com>
2022-02-02 16:55:52 -0500

    Creating Dynamic Tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,62 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
+
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+}
+
+const createTweetElement = function(tweet) {
+let $tweet = `
+  <article class="tweets">
+    <header>
+      <span class="profile">
+        <img class="profile-pic" src="${tweet.user.avatars}"> 
+      <span>${tweet.user.name}</span>
+        </span>
+      <span class="user">${tweet.user.handle}</span>
+    </header>
+      <span class="text">${tweet.content.text}</span>
+    <footer>
+      <span class="timestamp">${timeago.format(tweet.created_at)}</span>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+    </footer>
+  </article>
+`;
+  return $tweet;
+}
+
+renderTweets(tweetData);
+
+});
 
